1a30a6d1047ca2a3c055b840321c33ab3d70ef5e
gulromero <gulsumerd38@gmail.com>
2025-03-11 18:55:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/extensionfunction/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.extensionfunction
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.extensionfunction.ui.theme.ExtensionFunctionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ExtensionFunctionTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Extension1(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Extension1(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        val originalText = "hello"
+        val doubledText = originalText.double()
+
+        val palindromeText = "hi"
+        val isPalindrome = palindromeText.isPalindrome()
+        Text(text = palindromeText.isPalindrome()
+        )
+
+        Text(text = "Original: $originalText")
+        Text(text = "Doubled: $doubledText")
+
+        val sentence = "Gulsum was here"
+        val firstWord = sentence.theFirstWord()
+        Text(text = "Sentence: $sentence")
+        Text(text = "First Word: $firstWord")
+2
+
+
+    }
+
+}
+
+// Extension Function for String
+fun String.double(): String {
+    return this + this
+}
+
+//Extension Function for Palindrome words
+fun String.isPalindrome(): Boolean {
+    return this == this.reversed()
+}
+
+//Extension Function for Returning first word in Stringg
+fun String.theFirstWord(): String {
+    return this.split(" ").first()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ExtensionFunctionTheme {
+        Extension1()
+    }
+}
